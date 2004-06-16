7ac38a00c3b4a09ec4e25af0acac995334be4167
nad <unknown>
2004-06-16 10:41:21 +0000

    Updated comments.

--- ChasingBottoms/IsType.hs
+++ ChasingBottoms/IsType.hs
@@ -4,16 +4,18 @@ module ChasingBottoms.IsType( isFunction, isTuple, isList, isString ) where
 
 import Data.Typeable
 
--- isFunction f returns True iff the top level "constructor" of f is a
--- function arrow.
+-- | '@isFunction@ f' returns 'True' iff the top level \"constructor\"
+-- of @f@ is a function arrow.
 isFunction :: Typeable a => a -> Bool
 isFunction f = con f == con not  -- TyCon is abstract.
 
 con :: Typeable a => a -> TyCon
 con = typerepTyCon . typeOf
 
--- This function is rather fragile, but should be OK.
--- The unit type is not considered to be a tuple.
+-- | This function is rather fragile, but should be OK. It is only
+-- used by "ChasingBottoms.ApproxShow", which should only be used for
+-- debugging purposes anyway. The unit type is not considered to be a
+-- tuple.
 isTuple :: Typeable a => a -> Bool
 isTuple x = if null s then False else head s == ','
   where s = tyconString (con x)
